763cf5ff48fb07a695975e89270726b3b191505a
Geraldine Chauca <gchaucae@gmail.com>
2017-04-19 12:50:33 -0500

    Arcos y centro

--- js/main.js
+++ js/main.js
@@ -2,9 +2,24 @@
 var caja1 = document.createElement('div');
 caja1.id = 'contenedor1';
 
-var caja2 = document.createElement('div');
-caja2.id = 'contenedor2';
-
 var body = document.querySelector('body');
 body.appendChild(caja1);
-body.appendChild(caja2);
+
+//Elementos internos
+var borde = document.createElement('div');
+borde.id = 'borde';
+caja1.appendChild(borde);
+
+var arco1 = document.createElement('div');
+arco1.id = 'arco1';
+borde.appendChild(arco1);
+
+var centro = document.createElement('div');
+centro.id = 'centro';
+borde.appendChild(centro);
+
+var arco2 = document.createElement('div');
+arco2.id = 'arco2';
+borde.appendChild(arco2);
+
+
