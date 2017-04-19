69ee71fa358f9395cc8bea00362cae501b0b8688
Geraldine Chauca <gchaucae@gmail.com>
2017-04-19 11:54:19 -0500

    Archivos base

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Cajas base
+var caja1 = document.createElement('div');
+caja1.id = 'contenedor1';
+
+var caja2 = document.createElement('div');
+caja2.id = 'contenedor2';
+
+var body = document.querySelector('body');
+body.appendChild(caja1);
+body.appendChild(caja2);
